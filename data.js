c4c4dea9c02cf30365ed1cbfee94248fd4b638af
kuacoco <kuacoco@qq.com>
2024-05-22 09:31:17 +0800

    格式化

--- data.js
+++ data.js
@@ -1,146 +1,146 @@
 export default [
-    {
-        "title": "天鹅舞步",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202405/baae83bf5ef4464f9416a9c40341ae86.mp4"
-    },
-    {
-        "title": "魔法舞会",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202404/62deabd0a67940dd9f7512be31d428a8.mp4"
-    },
-    {
-        "title": "冰雪滑雪场",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202404/a67005cc335e49b3b84ee53fd6ffcd32.mp4"
-    },
-    {
-        "title": "雪宝的诞生（单一）",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202404/899a0d009ff348c88484b7ea84b4bb2b.mp4"
-    },
-    {
-        "title": "扔雪球",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202403/90c1cf9060cf44d48171c5e06b9c97f9.mp4"
-    },
-    {
-        "title": "天鹅翅膀",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202403/4c858999ee66463189766e718990585e.mp4"
-    },
-    {
-        "title": "结识艾莎与安娜",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202403/af5dafab521140d19b6799c2b3e7a109.mp4"
-    },
-    {
-        "title": "萤火虫",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202401/cdfa457d7a7f4102be0fc677f8a93152.mp4"
-    },
-    {
-        "title": "最好的未来（单一）",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202312/35853f80bac645c090199ae968a6205f.mp4"
-    },
-    {
-        "title": "小鱼泡泡",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202312/8522b4c0dce34ae0be729a49a8c7af85.mp4"
-    },
-    {
-        "title": "小公主小王子",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202311/e2ffbb5ab38845ffb799779978177cb1.mp4"
-    },
-    {
-        "title": "公主驾到(无老师单独演示)",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202311/a4bd88d207e04ae0bb008a291aaf4cd8.mp4"
-    },
-    {
-        "title": "galop（单一动作）",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202310/8d4898095c6541718a376ce9e02a9d6c.mp4"
-    },
-    {
-        "title": "天使翅膀",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202309/0422fee1a1a74352aafa5078b6b311f9.mp4"
-    },
-    {
-        "title": "抓蝴蝶",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202309/5168c0c095ee498389e9dbacdabd5f32.mp4"
-    },
-    {
-        "title": "蛙蛙跳",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202309/f02ac118a1104d298e9ed90390d0e7ef.mp4"
-    },
-    {
-        "title": "warm up(无老师单独演示)",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202308/7fe3d56862ee4e0a970b02bb40aea744.mp4"
-    },
-    {
-        "title": "打招呼(无老师单独演示)",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202308/369b45f7d43f44e3aa5d48e52213fd71.mp4"
-    },
-    {
-        "title": "小蝌蚪(无老师单独演示)",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202308/5318edcec83a4c76b73aca6478713b90.mp4"
-    },
-    {
-        "title": "海边游戏",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202307/7f145c431deb4ca29910f0c023500d64.mp4"
-    },
-    {
-        "title": "开胯旁腰",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202307/99e686443ede40188e09634f7032ea0e.mp4"
-    },
-    {
-        "title": "化妆舞会",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202307/ddc355960b4e455eb8250efc1e1975a3.mp4"
-    },
-    {
-        "title": "小螃蟹",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202306/619e913499054e82b33f292c298adc67.mp4"
-    },
-    {
-        "title": "步伐合并",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202306/fa50e5c7b8ab410e8093368c6f38ba42.mp4"
-    },
-    {
-        "title": "拍手(无老师单独演示)",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202305/5dcb140eaef647a5a74fd3e1da1b4184.mp4"
-    },
-    {
-        "title": "最美的光",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202304/92f4993cab5e4fd59aefc1accb4e8111.mp4"
-    },
-    {
-        "title": "relax",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202303/fc66dc66a51f41269b0c2e858cfc30ab.mp4"
-    },
-    {
-        "title": "天鹅湖",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202303/e5e83a426a004a1d929b4f3e6df8746d.mp4"
-    },
-    {
-        "title": "剪刀石头布(无老师单独演示)",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/0ee063b1ce9a4ca7848e7937f4eecbfc.mp4"
-    },
-    {
-        "title": "甜甜圈",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/a8d8a0ba369a4041824ce9884fab97c1.mp4"
-    },
-    {
-        "title": "摘花",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/39af97e189a34582aaf967faf4bc34d5.mp4"
-    },
-    {
-        "title": "芭比",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/bcaaf562f9174d53b9c7d28f8affa9f7.mp4"
-    },
-    {
-        "title": "吹泡泡",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202212/70c1a913153c4a7b8e1bb5a3990852fb.mp4"
-    },
-    {
-        "title": "海中划小船",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202211/d39000c42ea74a89880d13214d937d2b.mp4"
-    },
-    {
-        "title": "摇铃鼓",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202211/59d0338ab6ce407e87127245b34f753a.mp4"
-    },
-    {
-        "title": "指令练习",
-        "href": "https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202210/52cecf0447f344e99ca1551c19ab740f.mp4"
-    }
+  {
+    title: '天鹅舞步',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202405/baae83bf5ef4464f9416a9c40341ae86.mp4',
+  },
+  {
+    title: '魔法舞会',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202404/62deabd0a67940dd9f7512be31d428a8.mp4',
+  },
+  {
+    title: '冰雪滑雪场',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202404/a67005cc335e49b3b84ee53fd6ffcd32.mp4',
+  },
+  {
+    title: '雪宝的诞生（单一）',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202404/899a0d009ff348c88484b7ea84b4bb2b.mp4',
+  },
+  {
+    title: '扔雪球',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202403/90c1cf9060cf44d48171c5e06b9c97f9.mp4',
+  },
+  {
+    title: '天鹅翅膀',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202403/4c858999ee66463189766e718990585e.mp4',
+  },
+  {
+    title: '结识艾莎与安娜',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202403/af5dafab521140d19b6799c2b3e7a109.mp4',
+  },
+  {
+    title: '萤火虫',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202401/cdfa457d7a7f4102be0fc677f8a93152.mp4',
+  },
+  {
+    title: '最好的未来（单一）',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202312/35853f80bac645c090199ae968a6205f.mp4',
+  },
+  {
+    title: '小鱼泡泡',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202312/8522b4c0dce34ae0be729a49a8c7af85.mp4',
+  },
+  {
+    title: '小公主小王子',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202311/e2ffbb5ab38845ffb799779978177cb1.mp4',
+  },
+  {
+    title: '公主驾到(无老师单独演示)',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202311/a4bd88d207e04ae0bb008a291aaf4cd8.mp4',
+  },
+  {
+    title: 'galop（单一动作）',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202310/8d4898095c6541718a376ce9e02a9d6c.mp4',
+  },
+  {
+    title: '天使翅膀',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202309/0422fee1a1a74352aafa5078b6b311f9.mp4',
+  },
+  {
+    title: '抓蝴蝶',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202309/5168c0c095ee498389e9dbacdabd5f32.mp4',
+  },
+  {
+    title: '蛙蛙跳',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202309/f02ac118a1104d298e9ed90390d0e7ef.mp4',
+  },
+  {
+    title: 'warm up(无老师单独演示)',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202308/7fe3d56862ee4e0a970b02bb40aea744.mp4',
+  },
+  {
+    title: '打招呼(无老师单独演示)',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202308/369b45f7d43f44e3aa5d48e52213fd71.mp4',
+  },
+  {
+    title: '小蝌蚪(无老师单独演示)',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202308/5318edcec83a4c76b73aca6478713b90.mp4',
+  },
+  {
+    title: '海边游戏',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202307/7f145c431deb4ca29910f0c023500d64.mp4',
+  },
+  {
+    title: '开胯旁腰',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202307/99e686443ede40188e09634f7032ea0e.mp4',
+  },
+  {
+    title: '化妆舞会',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202307/ddc355960b4e455eb8250efc1e1975a3.mp4',
+  },
+  {
+    title: '小螃蟹',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202306/619e913499054e82b33f292c298adc67.mp4',
+  },
+  {
+    title: '步伐合并',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202306/fa50e5c7b8ab410e8093368c6f38ba42.mp4',
+  },
+  {
+    title: '拍手(无老师单独演示)',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202305/5dcb140eaef647a5a74fd3e1da1b4184.mp4',
+  },
+  {
+    title: '最美的光',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202304/92f4993cab5e4fd59aefc1accb4e8111.mp4',
+  },
+  {
+    title: 'relax',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202303/fc66dc66a51f41269b0c2e858cfc30ab.mp4',
+  },
+  {
+    title: '天鹅湖',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202303/e5e83a426a004a1d929b4f3e6df8746d.mp4',
+  },
+  {
+    title: '剪刀石头布(无老师单独演示)',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/0ee063b1ce9a4ca7848e7937f4eecbfc.mp4',
+  },
+  {
+    title: '甜甜圈',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/a8d8a0ba369a4041824ce9884fab97c1.mp4',
+  },
+  {
+    title: '摘花',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/39af97e189a34582aaf967faf4bc34d5.mp4',
+  },
+  {
+    title: '芭比',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202302/bcaaf562f9174d53b9c7d28f8affa9f7.mp4',
+  },
+  {
+    title: '吹泡泡',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202212/70c1a913153c4a7b8e1bb5a3990852fb.mp4',
+  },
+  {
+    title: '海中划小船',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202211/d39000c42ea74a89880d13214d937d2b.mp4',
+  },
+  {
+    title: '摇铃鼓',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202211/59d0338ab6ce407e87127245b34f753a.mp4',
+  },
+  {
+    title: '指令练习',
+    href: 'https://cdn08.xiaogj.com/file/dc6b12a64c8e426f86df6f7f039a5720/202210/52cecf0447f344e99ca1551c19ab740f.mp4',
+  },
 ]
